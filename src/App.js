fa5a2589ffc18cf41bb24198743bb340be92bc37
rodrieri000 <ericarodriguez7@outlook.com>
2020-05-23 21:53:57 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '', 
+      todos: [], 
+      isClicked: false
+    };
+  }
 
+  handleClick = () => {
+    this.setState({
+      isClicked: true,
+      todos: [...this.state.todos, this.state.text], 
+      text: ''
+    });
+  }
+
+  onChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  };
+
+  handleDelete = (index) => () => {
+    const todosCopy = [...this.state.todos];
+    todosCopy.splice(index, 1);
+
+    this.setState({
+      todos: todosCopy,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input value={this.state.text} onChange={this.onChange}></input>
+          <button onClick={this.handleClick}>{this.state.isClicked ? 'True': 'False'}</button>
+
+          {this.state.todos.map((todo, index) => (
+            <div key={index}>
+              <button onClick={this.handleDelete(index)}>Delete</button>
+              {todo}
+            </div>
+          ))}
+        </header>
+      </div>
+    );
+  }
+
+
+}
 export default App;
